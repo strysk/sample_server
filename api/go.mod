1d8fa2adee35523415bff3df392f8d4ef685281b
strysk <45932261+strysk@users.noreply.github.com>
2024-01-03 00:29:40 +0900

    databaseとの繋ぎこみ (#4)

--- api/go.mod
+++ api/go.mod
@@ -2,4 +2,8 @@ module sample_server/api
 
 go 1.21.5
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+)
--- api/main.go
+++ api/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"sample_server/api/usecase"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Printf("could not load env: %v", err)
+	}
+
+	Init()
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/posts", usecase.GetPosts).Methods("GET")
-	router.HandleFunc("/posts", usecase.CreatePost).Methods("POST")
-	router.HandleFunc("/posts/{id}", usecase.GetPost).Methods("GET")
-	router.HandleFunc("/posts/{id}", usecase.UpdatePost).Methods("PUT")
-	router.HandleFunc("/posts/{id}", usecase.DeletePost).Methods("DELETE")
+	router.HandleFunc("/posts", GetPosts).Methods("GET")
+	router.HandleFunc("/posts", CreatePost).Methods("POST")
+	router.HandleFunc("/posts/{id}", GetPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", UpdatePost).Methods("PUT")
+	router.HandleFunc("/posts/{id}", DeletePost).Methods("DELETE")
 
 	http.ListenAndServe(":8000", router)
 }
